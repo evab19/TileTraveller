aa153c1d9084f4f7b8d78f2865aa24d14130be62
Eva <evab19@ru.is>
2019-09-17 19:49:13 +0000

    alost ready code

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,11 +1,94 @@
-# starting position is 1,1. 
-#       print possible directions
-#       user input decides direction
-# north = x, y+1
-# south = x, y-1
-# east = x+1, y
-# west = x-1, y
-# x max 3
-# y max 3
-# position 3,1 = victory!
 
+[x,y] = [1,1]
+ 
+
+while [x,y] != [3,1]:
+    
+    if [x,y] == [1,1]:
+        print("You can travel: (N)orth.")
+        direction = str.lower(input("Direction: "))
+        if direction == "n":
+            y += 1
+        else:
+            print("Not a valid direction!")
+            
+
+
+    elif [x,y] == [1,2]:
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+        direction = str.lower(input("Direction: "))
+        if direction == "n":
+            y += 1
+        elif direction == "e":
+            x += 1
+        elif direction == "s":
+            y -= 1
+        else: 
+            print("Not a valid direction")
+            
+    
+    elif [x,y] == [1,3]:
+        print("You can travel: (E)ast or (S)outh.")
+        direction = str.lower(input("Direction: "))
+        if direction == "s":
+            y -= 1
+        if direction == "e":
+            x +=  1
+        else:
+            print("Not a valid direction!")
+            
+    
+    elif [x,y] == [2,1]:
+        print("You can travel: (N)orth.")
+        direction = str.lower(input("Direction: "))
+        if direction == "n":
+            y += 1
+        else:
+            print("Not a valid direction!")
+            
+
+    elif [x,y] == [2,2]:
+        print("You can travel: (S)outh or (W)est.")
+        direction = str.lower(input("Direction: "))
+        if direction == "s":
+            y -= 1
+        elif direction == "w":
+            x -= 1
+        else:
+            print("Not a valid direction!")
+            
+
+    elif [x,y] == [2,3]:
+        print("You can travel: (E)ast or (W)est.")
+        direction = str.lower(input("Direction: "))
+        if direction == "e":
+            x += 1
+        elif direction == "w":
+            x -= 1
+        else:
+            print("Not a valid direction!")
+            
+
+    elif [x,y] == [3,3]:
+        print("You can travel: (S)outh or (W)est.")
+        direction = str.lower(input("Direction: "))
+        if direction == "w":
+            x -= 1
+        elif direction == "s":
+            y -= 1
+        else:
+            print("Not a valid direction!")
+            
+
+    elif [x,y] == [3,2]:
+        print("You can travel: (N)orth or (S)outh.")
+        direction = str.lower(input("Direction: "))
+        if direction == "n":
+            y += 1
+        elif direction == "s":
+            y -= 1
+        else:
+            print("Not a valid direction!")
+            
+
+print("Victory!")
