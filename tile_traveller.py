1b5ee99d06baa8ad4a5973ad4c70f4e2d0c13a54
Eva <evab19@ru.is>
2019-09-17 18:08:29 +0000

    hæ

---
+++ tile_traveller.py
@@ -0,0 +1,11 @@
+# starting position is 1,1. 
+#       print possible directions
+#       user input decides direction
+# north = x, y+1
+# south = x, y-1
+# east = x+1, y
+# west = x-1, y
+# x max 3
+# y max 3
+# position 3,1 = victory!
+
